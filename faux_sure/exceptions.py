7949c8124495e1a5b04ff885a7e4bc118a92d760
arjun annapureddy <arjun.annapureddy@gmail.com>
2021-03-06 08:59:50 -0600

    limit context of model exceptions

--- faux_sure/exceptions.py
+++ faux_sure/exceptions.py
@@ -1,18 +1,37 @@
-class ModelNotFoundError(Exception):
+import sys
+
+
+class REPLException(Exception):
+    """Suppress traceback to maintain flow of Ipython"""
+
+    __suppress_context__ = True
+
+
+class ModelNotFoundError(REPLException):
     pass
 
 
-class UniqueFieldRequirementException(Exception):
+class UniqueFieldRequirementException(REPLException):
     pass
 
 
-class TypeFieldRequirementException(Exception):
+class TypeFieldRequirementException(REPLException):
     pass
 
 
-class ValidatorFieldRequirementException(Exception):
+class ValidatorFieldRequirementException(REPLException):
     pass
 
 
-class OneToOneException(Exception):
+class OneToOneException(REPLException):
     pass
+
+
+def repl_hook(kind, message, traceback):
+    if REPLException in kind.__bases__:
+        print("{0}: {1}".format(kind.__name__, message))  # Only print Error Type and Message
+    else:
+        sys.__excepthook__(kind, message, traceback)  # Print Error Type, Message and Traceback
+
+
+sys.excepthook = repl_hook
